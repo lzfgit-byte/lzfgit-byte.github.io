d3df340f12e5aa37e88422e44b4631958d1aadad
180745 <1807409742@qq.com>
2021-09-06 23:14:48 +0800

    修改

--- js/index.js
+++ js/index.js
@@ -21,19 +21,25 @@ let indexObj = {
     ,initData:function () {
         let webs = data.webs;
         let html = "";
+        let count = 0;
         data.webs.forEach((value,index)=>{
             if(value.show !== false){
                 if(value.src == undefined){
                     value.src = data.defaultImg;
                 }
 
-                let temp = `<li data_old="1" class="sigleRecommond"> <span old_del="${(index+1)}" class="del-flag glyphicon glyphicon-remove"></span> 
+                let temp = `<li data_old="1" class="sigleRecommond"> <span old_del="${++count}" class="del-flag glyphicon glyphicon-remove"></span> 
                  <a class="singleLink" href="${value.href}" title="" target="_blank"> <img class="img-links2" src="${value.src}" 
-                style="box-shadow: 0 10px 10px -6px #3c3737bf;border-radius: 45%;padding: 10% 10% 10% 10%"> <span class="text-links">${value.name}</span> </a> </li>`;
+                style="box-shadow: 0 10px 10px -6px #3c3737bf;border-radius: 50%;padding: 10% 10% 10% 10%"> <span class="text-links">${value.name}</span> </a> </li>`;
                 html += temp
             }
-
         })
+        for(let i = count;i<50;i++){
+            let temp = `<li data_old="1" class="sigleRecommond"> <span old_del="${i}" class="del-flag glyphicon glyphicon-remove"></span> 
+                 <a class="singleLink" href="#" title="" target="_blank"> <img class="img-links2"  
+                style="box-shadow: 0 10px 10px -6px #3c3737bf;border-radius: 50%;padding: 10% 10% 10% 10%"> <span class="text-links">待定</span> </a> </li>`;
+            html += temp
+        }
         $("#dataContains").html(html);
     }
     ,init:function () {
