a08ca3911252a2907dfb3041d5a2a358c29cb6f2
lzfgit <1807409742@qq.com>
2021-09-06 15:31:45 +0800

    排版

--- js/index.js
+++ js/index.js
@@ -1,3 +1,5 @@
+import {data} from '../data.js'
+
 let indexObj = {
     bind:function () {
         $("#q-btn").on("mouseover",function () {
@@ -12,8 +14,21 @@ let indexObj = {
             $("#q-word").val($(this).val())
         })
     }
+    ,initData:function () {
+        let webs = data.webs;
+        let html = "";
+        data.webs.forEach((value,index)=>{
+            let temp = `<li data_old="1" class="sigleRecommond"> <span old_del="${(index+1)}" class="del-flag glyphicon glyphicon-remove"></span> 
+                 <a class="singleLink" href="${value.href}" title="" target="_blank"> <img class="img-links2" src="${value.src}" 
+            style="box-shadow: 0 10px 10px -6px rgba(225, 0, 0, 0.75);"> <span class="text-links">${value.name}</span> </a> </li>`;
+
+            html += temp
+        })
+        $("#dataContains").html(html);
+    }
     ,init:function () {
         this.bind()
+        this.initData()
     }
 
 };
