4d81efd974043f5832c4de4f5763a73d83ad96bd
180745 <1807409742@qq.com>
2021-09-22 23:15:18 +0800

    change

--- popup.js
+++ popup.js
@@ -1,32 +1,54 @@
 // "chrome_url_overrides": {
 //   "newtab": "index.html"
 // },
-let changeColor = document.getElementById("changeColor");
 let form = layui.form;
 
-chrome.storage.sync.get("color", ({ color }) => {
+let popup = {
+    init: function () {
+        //保存当前页面的数据
+        this.bind();
+        this.initWebDomain();
+    },
+    constValue: {},
+    bind:function () {
+        $('#setThisWeb').click(function () {
+            getActiveTable(function () {
+                localStorage.setItem("text","456456")
+            })
+        })
+        $('#saveThisWeb').click(function () {
+            debugger
+            getActiveTable(function () {
+                localStorage.getItem("text");
+            })
+        })
+    },
+    initWebDomain:async function () {
+        let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+        $("#domain").val(new URL(tab.url).host);
+        chrome.scripting.executeScript({
+            target: { tabId: tab.id },
+            function: function () {
+                chrome.storage.sync.get("colorMy", ({ colorMy }) => {
+                    localStorage.setItem("text",colorMy);
+                });
+            },
+        });
 
-  changeColor.style.backgroundColor = color;
-});
 
-// When the button is clicked, inject setPageBackgroundColor into current page
-changeColor.addEventListener("click", async () => {
-  let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
-
-  chrome.scripting.executeScript({
-    target: { tabId: tab.id },
-    function: setPageBackgroundColor,
-  });
-});
-
-// The body of this function will be execuetd as a content script inside the
-// current page
-function setPageBackgroundColor() {
-  chrome.storage.sync.get("color", ({ color }) => {
-    // document.body.style.backgroundColor = color;
-
-    localStorage.setItem("6666",color);
+    }
+};
 
 
-  });
+popup.init();
+
+
+
+async function getActiveTable(func) {
+    let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+    popup.constValue.tabinfo = tab;
+    chrome.scripting.executeScript({
+        target: { tabId: tab.id },
+        function: func,
+    });
 }
