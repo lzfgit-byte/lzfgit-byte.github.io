dee4c20000537186d0900a918291694099502cb0
180745 <1807409742@qq.com>
2021-09-25 21:08:34 +0800

    xiuagi

--- popup.js
+++ popup.js
@@ -7,33 +7,103 @@ let popup = {
     init: function () {
         //保存当前页面的数据
         this.bind();
-        this.initWebDomain();
+        this.initValues();
     },
     constValue: {},
     bind:function () {
-        $('#setThisWeb').click(function () {
-            getActiveTable(function () {
-                localStorage.setItem("text","456456")
-            })
+        $('#setThisWeb').click(async function () {
+            let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+            let webkey = new URL(tab.url).host.replaceAll(".","");
+            $("#domain").val(webkey);
+            chrome.scripting.executeScript({
+                target: { tabId: tab.id },
+                function: function () {
+                    let urll = new URL(window.location.href);
+                    let webKey  = urll.host;
+                    webKey = webKey.replaceAll(".","");
+                    let webkey2 = webKey+"_"
+                    chrome.storage.sync.get(webkey2, (val) => {
+                       if(val[webkey2]){
+                           chrome.storage.sync.get(val[webkey2],(va2)=>{
+                              let item = va2[val[webkey2]];
+                               localStorage.setItem(val[webkey2],item);
+                               window.location.href = window.location.href;
+                               return
+                           })
+                           return
+                       }
+                       //默认值得情况下 ，没有设置自己的key
+                        chrome.storage.sync.get(webKey,(va2)=>{
+                            let item = va2[webKey];
+                            localStorage.setItem(webKey,item);
+                            return
+                        })
+
+                    });
+                },
+            });
+
+
         })
-        $('#saveThisWeb').click(function () {
-            debugger
-            getActiveTable(function () {
-                localStorage.getItem("text");
+        $('#saveThisWeb').click(async function () {
+            let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+            let webkey = new URL(tab.url).host.replaceAll(".","");
+            webkey += "_"
+            $("#domain").val(webkey);
+            let asyncKey = $('#asyncKey').val();
+            if(asyncKey){
+                //如果设置了asyncKey
+                let saveKey = {};
+                saveKey[webkey] = asyncKey;
+                chrome.storage.sync.set(saveKey);
+            }
+            chrome.scripting.executeScript({
+                target: { tabId: tab.id },
+                function: function () {
+                    let urll = new URL(window.location.href);
+                    let webKey  = urll.host;
+                    webKey = webKey.replaceAll(".","");
+                    let webKey2 = webKey + "_";
+                    chrome.storage.sync.get(webKey2,(val)=>{
+                        if(val[webKey2]){//设置了这个网站的key
+                            let item = localStorage.getItem(val[webKey2]);
+                            let saveItem = {};
+                            saveItem[val[webKey2]] = item;
+                            chrome.storage.sync.set(saveItem);
+                            alert("保存成功")
+                            return;
+                        }
+                        let item = localStorage.getItem(webKey);
+                        let saveItem = {};
+                        saveItem[webKey] = item;
+                        alert("保存成功")
+                        chrome.storage.sync.set(saveItem);
+                    })
+                },
             })
         })
     },
-    initWebDomain:async function () {
+    initValues:async function () {
         let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
-        $("#domain").val(new URL(tab.url).host);
-        chrome.scripting.executeScript({
-            target: { tabId: tab.id },
-            function: function () {
-                chrome.storage.sync.get("colorMy", ({ colorMy }) => {
-                    localStorage.setItem("text",colorMy);
-                });
-            },
-        });
+        let webkey = new URL(tab.url).host.replaceAll(".","");
+        let webKey2 = webkey+"_"
+        $("#domain").val(webkey);
+
+        chrome.storage.sync.get(webKey2,(val)=>{
+            if(val[webKey2]){//设置了这个网站的key
+               $('#domiankey').val(val[webKey2])
+                let dataJey = val[webKey2];
+                chrome.storage.sync.getBytesInUse(dataJey,(data)=>{
+                    $("#sizeSync").val(data + "/" + chrome.storage.sync.QUOTA_BYTES_PER_ITEM)
+                })
+                return
+            }
+            chrome.storage.sync.getBytesInUse(webkey,(data)=>{
+                $("#sizeSync").val(data + "/" + chrome.storage.sync.QUOTA_BYTES_PER_ITEM + "")
+            })
+
+        })
+
 
 
     }
@@ -43,12 +113,3 @@ let popup = {
 popup.init();
 
 
-
-async function getActiveTable(func) {
-    let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
-    popup.constValue.tabinfo = tab;
-    chrome.scripting.executeScript({
-        target: { tabId: tab.id },
-        function: func,
-    });
-}
